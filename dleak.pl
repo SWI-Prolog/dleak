9337c8750d7bcd5da5723d8fc429df1d6dd6cefc
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-05-06 16:54:50 +0200

    Keep totals per allocation context

--- dleak.pl
+++ dleak.pl
@@ -23,6 +23,7 @@ process(In) :-
 				 realloc:0,
 				 free:0,
 				 total:0,
+				 context_info:context{},
 				 wtime:0
 			       }).
 
@@ -40,13 +41,31 @@ process(end_of_file, _, _) :- !.
 process(Term, In, State) :-
 	action(Term, State),
 	inc(events, State, 1),
-	(   State.events mod 10000 =:= 0
+	(   State.events mod 100000 =:= 0
 	->  wtime(State, Time),
-	    format(user_error, '~p (~D)~n', [State, Time])
+	    dump_state(State, Time)
 	;   true
 	),
 	read(In, Term2),
 	process(Term2, In, State).
+
+dump_state(State, Time) :-
+	format('Events: ~D, memory: ~D (~D msec)~n',
+	       [State.events, State.total, Time]),
+	dict_pairs(State.context_info, _, Pairs),
+	transpose_pairs(Pairs, BySize),
+	reverse(BySize, Decreasing),
+	list_contexts(Decreasing, 10),
+	nl.
+
+list_contexts([], _).
+list_contexts(_, 0) :- !.
+list_contexts([Mem-Ctx|T], N) :-
+	format(' ~d: ~D; ', [Ctx, Mem]),
+	N2 is N - 1,
+	list_contexts(T, N2).
+
+
 
 :- dynamic
 	cc/2,				% calling context
@@ -62,26 +81,39 @@ inc(Field, State, Extra) :-
 	New is State.Field+Extra,
 	nb_set_dict(Field, State, New).
 
+inc_usage(State, Ctx, Extra) :-
+	get_dict(context_info, State, Info),
+	(   get_dict(Ctx, Info, Old)
+	->  New is Old + Extra,
+	    nb_set_dict(Ctx, Info, New)
+	;   put_dict(Ctx, Info, Extra, NewInfo),
+	    nb_set_dict(context_info, State, NewInfo)
+	).
+
 action(cc(Id, Stack), State) :-
 	inc(contexts, State, 1),
 	assertz(cc(Id, Stack)).
 action(malloc(Ctx,Size,Ptr), State) :-
 	inc(malloc, State, 1),
 	inc(total, State, Size),
+	inc_usage(State, Ctx, Size),
 	asserta(chunk(Ptr,Size,Ctx)).
 action(calloc(Ctx,N,Len,Ptr), State) :-
 	Size is N*Len,
 	inc(calloc, State, 1),
 	inc(total, State, Size),
+	inc_usage(State, Ctx, Size),
 	asserta(chunk(Ptr,Size,Ctx)).
 action(realloc(Ctx,Ptr,Size,NPtr), State) :-
 	inc(realloc, State, 1),
 	(   Ptr == nil
 	->  inc(total, State, Size),
+	    inc_usage(State, Ctx, Size),
 	    asserta(chunk(NPtr,Size,Ctx))
-	;   retract(chunk(Ptr,OSize,_))
+	;   retract(chunk(Ptr,OSize,OrigCtx))
 	->  Added is Size-OSize,
 	    inc(total, State, Added),
+	    inc_usage(State, OrigCtx, Added),
 	    asserta(chunk(NPtr,Size,Ctx))
 	;   print_message(error, realloc(Ctx,Ptr,Size,NPtr)),
 	    abort
@@ -90,8 +122,10 @@ action(free(Ctx,Ptr), State) :-
 	inc(free, State, 1),
 	(   Ptr == nil
 	->  true
-	;   retract(chunk(Ptr,OSize,_))
-	->  inc(total, State, -OSize)
+	;   retract(chunk(Ptr,OSize,AllocCtx))
+	->  Freed is -OSize,
+	    inc_usage(State, AllocCtx, Freed),
+	    inc(total, State, Freed)
 	;   print_message(error, free(Ctx,Ptr))
 	).
 
@@ -130,6 +164,10 @@ prolog:message(realloc(Ctx,Ptr,_Size,_NPtr)) -->
 prolog:message(free(Ctx,Ptr)) -->
 	{ cc(Ctx, Stack) },
 	[ 'free() of unknown pointer 0x~16r'-[Ptr], nl ],
+	context(Stack).
+prolog:message(cc(Ctx)) -->
+	{ cc(Ctx, Stack) },
+	[ 'Calling context:'-[], nl ],
 	context(Stack).
 
 context(Stack) -->
